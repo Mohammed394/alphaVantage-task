4616315771e613097655b8d85bf779e5de69c98d
Mohammed394 <mohammed.aly.ismail@gmail.com>
2025-09-07 17:15:28 +0300

    add fundamentalTest

---
+++ pages/alphavantage-apis/categories/fundamental.category.ts
@@ -0,0 +1,13 @@
+import { createAPIContext } from "../../../utils/api-context";
+import { companyOverviewRequest } from "../models/requests/fundamentalRequest";
+import { companyOverviewResponse } from "../models/responses/fundamentalResponse";
+
+export async function getCompanyOverviewData(
+  payload: companyOverviewRequest
+): Promise<companyOverviewResponse> {
+  const api = await createAPIContext();
+  const apiKey = process.env.API_KEY;
+  const url = `${process.env.BASE_URL}?function=${payload.function}&symbol=${payload.symbol}&apikey=${apiKey}`;
+  const response = await api.get<companyOverviewResponse>(url);
+  return response;
+} 
